a6cd480c084008a4b25bafa989064d8c51de8e3d
MHDNezam <nizam.94@hotmail.com>
2020-10-08 08:25:11 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,7 @@
+// need to import a helper library from flutter
+
+// define a main fundtion to run when our app start
+
+// create a new text widget to show some text on the screen
+
+// taken that widget and get it on the screen
